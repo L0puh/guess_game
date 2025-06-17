78b381a41e8fa1f490969ba297aad0bde6de287e
L0puh <1opuhart@gmail.com>
2025-06-17 18:05:00 +0900

    fixes

--- utils/update_res.py
+++ utils/update_res.py
@@ -1,8 +1,6 @@
 import serial
 
-
 PORT = "/dev/ttyUSB0"
-
 def update_score(ser):
     try:
         with open("data/score.txt", "a") as f:
@@ -10,18 +8,27 @@ def update_score(ser):
                 line = ser.readline()
                 if line:
                     line = line.decode("utf-8").strip()
+
                     print("New data:", line)
                     f.write(line + "\n")
+    except KeyboardInterrupt:
+        print("\nTerminated by user")
     except Exception as e:
         print("Error in opening file:")
         print(e)
+    finally:
+        close_serial(ser)
+
+def close_serial(ser):
+    if ser.is_open:
+        ser.close()
+    print("Serial port is closed")
 def open_serial():
     ser = -1
     try:
         ser = serial.Serial(PORT, 9600, timeout=1)
         if ser.is_open:
             print("Serial is open. Receiving data...")  
-
     except serial.SerialException as e:
         print("Error in opening serial port:")
         print(e)
